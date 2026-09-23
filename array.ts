4ab100edd478d3c567abf96ad2c0ad0f4749cb94
Vansh <vanshchoudhary2744@gmail.com>
2026-09-23 20:33:09 +0530

    is array sorted

--- array.ts
+++ array.ts
@@ -16,21 +16,34 @@
 
 //--------------Second Largest Element---------
 
-function secondLargest(arr: number[]): number {
-  let fl = -Infinity;
-  let sl = -Infinity;
+// function secondLargest(arr: number[]): number {
+//   let fl = -Infinity;
+//   let sl = -Infinity;
 
-  for (let i of arr) {
-    if (i > fl) {
-      sl = fl;
-      fl = i;
-    }
-    if (i > sl && i !== fl) {
-      sl = i;
+//   for (let i of arr) {
+//     if (i > fl) {
+//       sl = fl;
+//       fl = i;
+//     }
+//     if (i > sl && i !== fl) {
+//       sl = i;
+//     }
+//   }
+
+//   return sl;
+// }
+
+// console.log(secondLargest([23, 56, 78, 90, 12, 10, 20, 89]));
+
+//----------------Is Array Sorted-----------------
+
+function isArraySorted(arr: number[]): boolean {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
   }
-
-  return sl;
+  return true;
 }
 
-console.log(secondLargest([23, 56, 78, 90, 12, 10, 20, 89]));
+console.log(isArraySorted([10, 2, 3, 4, 5, 6]));
