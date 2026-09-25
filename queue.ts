de2306a064b196a3e5e4157bf1b4c9a9c5d7ee90
Vansh <vanshchoudhary2744@gmail.com>
2026-09-25 18:12:17 +0530

    reverse k elements of queue

--- queue.ts
+++ queue.ts
@@ -45,72 +45,105 @@
 
 //------------Implement Queue using Stack----------------
 
-class Queue {
-  stack1: number[];
-  stack2: number[];
-  constructor() {
-    this.stack1 = [];
-    this.stack2 = [];
-  }
+// class Queue {
+//   stack1: number[];
+//   stack2: number[];
+//   constructor() {
+//     this.stack1 = [];
+//     this.stack2 = [];
+//   }
 
-  enqueue(num: number) {
-    this.stack1.push(num);
-  }
+//   enqueue(num: number) {
+//     this.stack1.push(num);
+//   }
 
-  dequeue(): number {
-    if (this.stack2.length === 0) {
-      while (this.stack1.length !== 0) {
-        this.stack2.push(this.stack1.pop() as number);
-      }
-      return this.stack2.pop() as number;
-    }
+//   dequeue(): number {
+//     if (this.stack2.length === 0) {
+//       while (this.stack1.length !== 0) {
+//         this.stack2.push(this.stack1.pop() as number);
+//       }
+//       return this.stack2.pop() as number;
+//     }
 
-    return this.stack2.pop() as number;
-  }
+//     return this.stack2.pop() as number;
+//   }
 
-  front(): number {
-    if (this.stack2.length === 0) {
-      while (this.stack1.length !== 0) {
-        this.stack2.push(this.stack1.pop() as number);
-      }
-      return this.stack2[0];
-    }
+//   front(): number {
+//     if (this.stack2.length === 0) {
+//       while (this.stack1.length !== 0) {
+//         this.stack2.push(this.stack1.pop() as number);
+//       }
+//       return this.stack2[0];
+//     }
 
-    return this.stack2[0];
-  }
+//     return this.stack2[0];
+//   }
 
-  display() {
-    console.log("--------");
+//   display() {
+//     console.log("--------");
 
-    if (this.stack2.length === 0) {
-      while (this.stack1.length !== 0) {
-        this.stack2.push(this.stack1.pop() as number);
-      }
-      for (let num of this.stack2) {
-        console.log(num);
-      }
-      console.log("--------");
-      return;
-    }
+//     if (this.stack2.length === 0) {
+//       while (this.stack1.length !== 0) {
+//         this.stack2.push(this.stack1.pop() as number);
+//       }
+//       for (let num of this.stack2) {
+//         console.log(num);
+//       }
+//       console.log("--------");
+//       return;
+//     }
 
-    for (let num of this.stack2) {
-      console.log(num);
-    }
-    console.log("--------");
-  }
-}
+//     for (let num of this.stack2) {
+//       console.log(num);
+//     }
+//     console.log("--------");
+//   }
+// }
 
-const queue = new Queue();
+// const queue = new Queue();
 
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-queue.enqueue(40);
+// queue.enqueue(10);
+// queue.enqueue(20);
+// queue.enqueue(30);
+// queue.enqueue(40);
 
-queue.display();
+// queue.display();
 
-console.log(queue.front());
+// console.log(queue.front());
 
-console.log(queue.dequeue());
+// console.log(queue.dequeue());
 
-queue.display();
+// queue.display();
+
+//----------Reverse k elements of Queue----------
+
+
+// class Queue {
+//   stack: number[];
+
+//   constructor() {
+//     this.stack = [];
+//   }
+
+//   reverse(queue: number[], k: number): number[] {
+//     for (let i = 0; i < k; i++) {
+//       this.stack.push(queue.shift() as number);
+//     }
+
+//     while (this.stack.length) {
+//       queue.push(this.stack.pop() as number);
+//     }
+
+//     let remaining = queue.length - k;
+
+//     while (remaining--) {
+//       queue.push(queue.shift() as number);
+//     }
+
+//     return queue;
+//   }
+// }
+
+// const queue = new Queue();
+
+// console.log(queue.reverse([1, 2, 3, 4, 5, 6], 3));
